72735f607556c86fb32112b0f160bc4bf46c2274
Abdulkadir Celikkanat <abdcelikkanat@gmail.com>
2019-10-08 22:46:54 +0200

    arastir

--- run3.py
+++ run3.py
@@ -10,8 +10,10 @@ def save_emb(emb, N, dim, outputname):
             line = "{} {}\n".format(str(node), " ".join('1' if v else '0' for v in emb[node, :]))
             f.write(line)
 
+
 def _crc32_function(x):
     return crc32(x) & 0xffffffff
+
 
 dim = 1024
 filename = "citeseer_undirected"
@@ -22,8 +24,12 @@ g = nx.read_gml(graph_path)
 
 nb_list = [[] for _ in range(g.number_of_nodes())]
 for node in g.nodes():
+    nb_list[int(node)].append(str(node))
     for nb in nx.neighbors(g, node):
         nb_list[int(node)].append(str(nb))
+        # for nb_nb in nx.neighbors(g, nb):
+        #     nb_list[int(node)].append(str(nb_nb))
+
 
 srp = SimHashSRP(dim=dim, vocab_size=g.number_of_nodes(), hash_function=_crc32_function)
 
