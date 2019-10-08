9db9b20119930f8440b797e2e9e1c44d593f88ac
Abdulkadir Celikkanat <abdcelikkanat@gmail.com>
2019-10-08 16:52:49 +0200

    hadi be

--- deneme1.py
+++ deneme1.py
@@ -17,19 +17,49 @@ def cosine_similarity(v1, v2):
 
     return 1.0 - cdist(np.asarray([v1.get_binary()]), np.asarray([v2.get_binary()]), metric='cosine')[0][0]
 
+def cosine_similarity_sets(s1, s2):
+
+    return float(len(set(s1).intersection(set(s2)))) / np.sqrt(len(s1) * len(s2))
 
 
-v1 = SimHash(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p'], dim)
-v2 = SimHash(['a', 'b', 'c'], dim)
-v3 = SimHash(['a', 'b', 'd'], dim)
+def equality_prob(b1, b2):
+
+    return np.sum(b1 == b2) / float(len(b1))
+
+
+s1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+s11 = ['aa', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+s2 = ['a', 'b', 'c']
+s3 = ['a', 'b', 'd']
+s4 = ['a', 'x', 'y', 'z', 'b', 'c', 'd', 'd', 'e', 'k']
+
+v1 = SimHash(s1, dim)
+v11 = SimHash(s11, dim)
+v2 = SimHash(s2, dim)
+v3 = SimHash(s3, dim)
+v4 = SimHash(s4, dim)
 
 b1 = np.asarray(v1.get_binary())*1.0
+b11 = np.asarray(v11.get_binary())*1.0
 b2 = np.asarray(v2.get_binary())*1.0
 b3 = np.asarray(v3.get_binary())*1.0
+b4 = np.asarray(v4.get_binary())*1.0
+
 
 print(b1)
 print(b2)
 print(b3)
-print(cosine_similarity(v1, v2))
-print(cosine_similarity(v1, v3))
-print(cosine_similarity(v2, v3))
+print(b4)
+
+
+print(equality_prob(b3, b1))
+print(v1.hamming_distance(v3)/float(dim))
+
+print("-------------------------------------------")
+
+c = cosine_similarity_sets(s1=s1, s2=s3)
+t = 1.0 - (np.arccos(c) / np.pi)
+
+print(c)
+
+print("->", t)
---
+++ run3.py
@@ -0,0 +1,34 @@
+import networkx as nx
+from simhashsrp import *
+from learn_embedding import *
+
+
+def save_emb(emb, N, dim, outputname):
+    with open(outputname, 'w') as f:
+        f.write("{} {}\n".format(N, dim))
+        for node in range(N):
+            line = "{} {}\n".format(str(node), " ".join('1' if v else '0' for v in emb[node, :]))
+            f.write(line)
+
+def _crc32_function(x):
+    return crc32(x) & 0xffffffff
+
+dim = 1024
+filename = "citeseer_undirected"
+graph_path="../datasets/{}.gml".format(filename)
+output_path="./embeddings/{}_ego_dim={}.embedding".format(filename, dim)
+
+g = nx.read_gml(graph_path)
+
+nb_list = [[] for _ in range(g.number_of_nodes())]
+for node in g.nodes():
+    for nb in nx.neighbors(g, node):
+        nb_list[int(node)].append(str(nb))
+
+srp = SimHashSRP(dim=dim, vocab_size=g.number_of_nodes(), hash_function=_crc32_function)
+
+emb = np.zeros(shape=(g.number_of_nodes(), dim), dtype=np.float)
+for node in g.nodes():
+    emb[int(node), :] = srp.encode(nb_list[int(node)])
+
+save_emb(emb=emb, N=g.number_of_nodes(), dim=dim, outputname=output_path)
